b0074a80d06fae6bbbc8adc0501447aca8a54c7a
Jacob Lever <jacobianism@gmail.com>
2018-06-19 16:59:06 +0100

    Rename test file to match others

--- src/test/java/uk/co/jacoblever/TransferResourceTests.java
+++ src/test/java/uk/co/jacoblever/TransferResourceTests.java
@@ -19,7 +19,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class TransferResourceTest {
+public class TransferResourceTests {
 
     private HttpServer server;
     private WebTarget target;
